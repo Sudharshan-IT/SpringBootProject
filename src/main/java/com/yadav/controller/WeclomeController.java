a7aa5b4e36f404c697d24c0f1df27ab28cf6992b
Sudharshan-IT <sudharshanpingme@gmail.com>
2021-02-26 11:11:30 +0530

    Welcome Message Method Added In WelcomeController Class

--- src/main/java/com/yadav/controller/WeclomeController.java
+++ src/main/java/com/yadav/controller/WeclomeController.java
@@ -2,4 +2,9 @@ package com.yadav.controller;
 
 public class WeclomeController {
 
+	public String welcomeMessage(String user) {
+		String msg="Welcome To Git Hub Project Mr.\t";
+		return msg+user;
+	}
+	
 }
